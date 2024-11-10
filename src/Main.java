0ea87d4fbf4490be135b5fc584b6ae676ccb66da
CamiloSar <camilo.sarmiento.amado@gmail.com>
2024-11-09 23:27:12 -0500

    Main file test

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
+import Model.PGN;
+import Model.Partida;
+import View.VistaTablero;
 public class Main {
+    public static void main(String[] args) {
+        VistaTablero vista = new VistaTablero();
+        Partida partida = new Partida("TEST_FILES/file1.pgn");
+        for(int i = 0; i<partida.numeroMovimientos; i++){
+            vista.imprimirTablero(partida.obtenerTablero(i).toByteMatriz());
+        }
+    }
 }
